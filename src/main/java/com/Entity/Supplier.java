e40efc5cdb51a5f6fb8873373211c2b515e46cbf
Hwy486 <3438097932@qq.com>
2025-12-01 14:26:39 +0800

    小帅999

--- src/main/java/com/Entity/Supplier.java
+++ src/main/java/com/Entity/Supplier.java
@@ -1,64 +1,17 @@
 package com.Entity;
 
-public class Supplier {
-    private String supplyname;
-    private int supplierid;
-    private String supplierpostcode;
-    private String supplieraddress;
-    private String supplierphone;
-    private String supplieremail;
-    private String supplierfax;
-    private String supplierrelationer;
-    public Supplier(){
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    }
-    public Supplier(int supplierid,String supplyname,String supplieraddress,String supplierpostcode,String supplierphone,String supplierfax,String supplieremail,String supplierrelationer) {}
-    public String getSupplyname() {
-        return supplyname;
-    }
-    public void setSupplyname(String supplyname) {
-        this.supplyname = supplyname;
-    }
-    public int getSupplierid() {
-        return supplierid;
-    }
-    public void setSupplierid(int supplierid) {
-        this.supplierid = supplierid;
-    }
-    public String getSupplierpostcode() {
-        return supplierpostcode;
-    }
-    public void setSupplierpostcode(String supplierpostcode) {
-        this.supplierpostcode = supplierpostcode;
-    }
-    public String getSupplieraddress() {
-        return supplieraddress;
-    }
-    public void setSupplieraddress(String supplieraddress) {
-        this.supplieraddress = supplieraddress;
-    }
-    public String getSupplierphone() {
-        return supplierphone;
-    }
-    public void setSupplierphone(String supplierphone) {
-        this.supplierphone = supplierphone;
-    }
-    public String getSupplieremail() {
-        return supplieremail;
-    }
-    public void setSupplieremail(String supplieremail) {
-        this.supplieremail = supplieremail;
-    }
-    public String getSupplierfax() {
-        return supplierfax;
-    }
-    public void setSupplierfax(String supplierfax) {
-        this.supplierfax = supplierfax;
-    }
-    public String getSupplierrelationer() {
-        return supplierrelationer;
-    }
-    public void setSupplierrelationer(String supplierrelationer) {
-        this.supplierrelationer = supplierrelationer;
-    }
+@Data
+@NoArgsConstructor
+public class Supplier {
+    private Integer exesConId;         // 供应商ID（主键）
+    private String supName;            // 供应商名称
+    private String supAddress;         // 地址
+    private String postcode;           // 邮编
+    private String supTelephone;       // 电话
+    private String supFax;             // 传真
+    private String supRelationer;      // 联系人
+    private String supEmail;           // 邮箱
 }
