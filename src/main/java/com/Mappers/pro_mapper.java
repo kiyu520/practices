a19d9833e4ffcd0e8cdfc4a4882ffe1e8f2176a7
kiyu520 <2186925409@qq.com>
2025-12-01 14:35:30 +0800

    更新添加数据库

--- src/main/java/com/Mappers/pro_mapper.java
+++ src/main/java/com/Mappers/pro_mapper.java
@@ -1,14 +1,12 @@
 package com.Mappers;
 
-import com.Entity.products;
+import com.Entity.Product;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 
-import java.io.IOException;
-
 public interface pro_mapper {
-//    @Insert("insert into practice.products(prod_id, prod_name, price, `type`, quantity, sup_id) values (#{p.pro_id},#{p.pro_name),#{d},#{d},#{d},#{d})")
-//    public int add_product(@Param("p") products p);
+    @Insert("insert into practice.products(prod_id, prod_name, price, `type`, quantity, sup_id) values (#{p.pro_id},#{p.pro_name},#{p.price},#{p.type},#{p.quantity},#{p.sup_id})")
+    public int add_product_entity(@Param("p") Product p);
     @Insert("insert into practice.products(prod_id, prod_name, price, `type`, quantity, sup_id) values(#{pro_id},#{pro_name},#{price},#{type},#{quantity},#{sup_id})")
-    public int add_product(@Param("pro_id") int pro_id, @Param("pro_name") String pro_name, @Param("price") int price, @Param("type") String type,@Param("quantity") double quantity,@Param("sup_id") int sup_id);
+    public int add_product_args(@Param("pro_id") int pro_id, @Param("pro_name") String pro_name, @Param("price") int price, @Param("type") String type,@Param("quantity") double quantity,@Param("sup_id") int sup_id);
 }
