e0397e45f0ef50781310f81688e65a30e1b78cc1
kiyu520 <2186925409@qq.com>
2025-12-02 14:21:27 +0800

    User语法统一,注释掉邪恶的豆包史山

--- src/main/java/com/Entity/User.java
+++ src/main/java/com/Entity/User.java
@@ -1,42 +1,16 @@
 package com.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String username;
     private String password;
     private int userRole;
-/**
- * 用户类的构造方法
- * @param username 用户名
- * @param password 密码
- * @param userRole 用户角色
- */
-     public User(String username, String password, int userRole) {
-    // 使用传入的参数初始化用户名
-         this.username = username;
-    // 使用传入的参数初始化密码
-         this.password = password;
-    // 使用传入的参数初始化用户角色
-         this.userRole = userRole;
-     }
-     public String getUsername() {
-         return username;
-     }
-     public void setUsername(String username) {
-         this.username = username;
-     }
-     public String getPassword() {
-         return password;
-     }
-     public void setPassword(String password) {
-         this.password = password;
-     }
-     public int getUserRole() {
-         return userRole;
-     }
-     public void setUserRole(int userRole) {
-         this.userRole = userRole;
-     }
 }
--- src/main/java/com/Entity/test.java
+++
@@ -1,7 +0,0 @@
-package com.Entity;
-
-import org.junit.Test;
-
-public class test {
-
-}
