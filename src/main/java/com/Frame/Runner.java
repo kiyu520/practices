31f3f7ce0fc691e6568bb6e95920924a60ce25be
booah00 <283798219@qq.com>
2025-12-03 16:20:19 +0800

    更改runner为事件调度线程（EDT）

--- src/main/java/com/Frame/Runner.java
+++ src/main/java/com/Frame/Runner.java
@@ -2,8 +2,15 @@ package com.Frame;
 
 import com.Entity.User;
 
+import javax.swing.*;
+
 public class Runner {
     public static void main(String[] args) {
-        new LoginFrame();
+        // 在事件调度线程中运行Swing组件
+        SwingUtilities.invokeLater(() -> {
+            // 创建登录窗口
+            LoginFrame loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
+        });
     }
 }
